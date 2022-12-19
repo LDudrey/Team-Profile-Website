638f1fd796e38eab2df5c1849b4551b5ac98b36e
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-19 16:02:51 -0600

    joined array to fix commas showing on html

--- index.js
+++ index.js
@@ -8,53 +8,53 @@ const Intern = require("./lib/Intern");
 const { inherits } = require('util');
 const employees = [];
 
-function addManager() {}
+function addManager() { }
 inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the Team Managers name?',
-      //   validate(answer) {
-      //     if(!answer) {
-      //         return "Please, enter the Managers name!"
-      //     }
-      //     return true
-      // }
-    },
-    {
-      type: 'number',
-      name: 'id',
-      message: 'What is the Team Managers id number?',
-      //   validate(answer) {
-      //     if(!answer) {
-      //         return "Please, enter the Managers employee ID number!"
-      //     }
-      //     return true
-      // }
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is the Team Managers email address?',
-      //   validate(answer) {
-      //     if(!answer) {
-      //         return "Please, enter the Managers email address!"
-      //     }
-      //     return true
-      // }
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is the Team Managers office number?',
-      //   validate(answer) {
-      //     if(!answer) {
-      //         return "Please, enter the Managers office number!"
-      //     }
-      //     return true
-      // }
-    },
-  ])
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the Team Managers name?',
+    //   validate(answer) {
+    //     if(!answer) {
+    //         return "Please, enter the Managers name!"
+    //     }
+    //     return true
+    // }
+  },
+  {
+    type: 'number',
+    name: 'id',
+    message: 'What is the Team Managers id number?',
+    //   validate(answer) {
+    //     if(!answer) {
+    //         return "Please, enter the Managers employee ID number!"
+    //     }
+    //     return true
+    // }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the Team Managers email address?',
+    //   validate(answer) {
+    //     if(!answer) {
+    //         return "Please, enter the Managers email address!"
+    //     }
+    //     return true
+    // }
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is the Team Managers office number?',
+    //   validate(answer) {
+    //     if(!answer) {
+    //         return "Please, enter the Managers office number!"
+    //     }
+    //     return true
+    // }
+  },
+])
   .then(answers => {
     const newManager = new Manager(
       answers.name,
@@ -219,77 +219,89 @@ function generateIndex(answers) {
   for (let i = 0; i < answers.length; i++) {
     if (answers[i].getRole() === 'Manager') {
       const card = `
-      <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${answers[i].name}</h5>
-        <h3 class="card-text">Manager</h3>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${answers[i].id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
-        <li class="list-group-item">Office number: ${answers[i].officeNumber}</li>
-      </ul>
-      </div>
-      </div>
-      `
-      cards.push(card)
+<div class="col">
+  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+    <div class="card-body" style="background-color: #0d6efd">
+      <h3 class="card-title">${answers[i].name}</h3>
+      <h5 class="card-text">Manager</h5>
+    </div>
+    <ul class="list-group list-group-flush p-4" style="background-color: #ebebeb;">
+      <li class="list-group-item">ID: ${answers[i].id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
+      <li class="list-group-item">Office number: ${answers[i].officeNumber}</li>
+    </ul>
+  </div>
+</div>
+`
+      cards.push(card);
     } else if (answers[i].getRole() === 'Engineer') {
       const card = `
-      <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${answers[i].name}</h5>
-        <h3 class="card-text">Engineer</h3>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${answers[i].id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
-        <li class="list-group-item">GitHub: ${answers[i].gitHub}</li>
-      </ul>
-      </div>
-      </div>
-      `
-      cards.push(card)
+<div class="col">
+  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+    <div class="card-body" style="background-color: #0d6efd">
+      <h3 class="card-title">${answers[i].name}</h3>
+      <h5 class="card-text">Engineer</h5>
+    </div>
+    <ul class="list-group list-group-flush p-4" style="background-color: #ebebeb;">
+      <li class="list-group-item">ID: ${answers[i].id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
+      <li class="list-group-item">GitHub:<a href="https://github.com/${answers[i].gitHub}">${answers[i].gitHub}</a></li>
+    </ul>
+  </div>
+</div>
+`
+      cards.push(card);
     } else if (answers[i].getRole() === 'Intern') {
       const card = `
-      <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${answers[i].name}</h5>
-        <h3 class="card-text">Intern</h3>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${answers[i].id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
-        <li class="list-group-item">School: ${answers[i].school}</li>
-      </ul>
-      </div>
-      </div>
-      `
-      cards.push(card)
+<div class="col">
+  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+    <div class="card-body" style="background-color: #0d6efd">
+      <h3 class="card-title">${answers[i].name}</h3>
+      <h5 class="card-text">Intern</h5>
+    </div>
+    <ul class="list-group list-group-flush p-4" style="background-color: #ebebeb;">
+      <li class="list-group-item">ID: ${answers[i].id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${answers[i].email}" class="card-link">${answers[i].email}</a></li>
+      <li class="list-group-item">School: ${answers[i].school}</li>
+    </ul>
+  </div>
+</div>
+`
+      cards.push(card);
     }
   }
-  const html = `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-          <link rel="stylesheet" href="./styles.css" />
-          <title>Team Profiles</title>
-      </head>
-      <body>
-      <div class="container">
-      ${cards}
-      </div>   
-      </body>
-      </html>`
+  let joinedCards = cards.join('');
+  const html = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+   integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css" />
+  <title>Team Profiles</title>
+</head>
+
+<body>
+<div class="row" style="background-color: #dc3545;">
+<h1>My Team</h1>
+</div>
+  <div class="container p-5">
+    <div class="row">${joinedCards}
+      </div>
+    </div>   
+  </body>
+</html>
+`
   return html;
 };
 
 // Function initializes app
 function init() {
- addManager();
+  addManager();
 };
 
 // Function call to initialize app
